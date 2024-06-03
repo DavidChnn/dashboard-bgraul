0f9a61fcaa9657ddafded953fb19c275b72b8b2f
satria29pamungkas@gmail.com <satria29pamungkas@gmail.com>
2024-06-03 17:36:00 +0700

    migration fix

--- database/migrations/2024_06_03_102901_create_asset_table.php
+++ database/migrations/2024_06_03_102901_create_asset_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cip', function (Blueprint $table) {
+        Schema::create('asset', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('assetCodeAccounting')->nullable();;
@@ -44,10 +44,6 @@ return new class extends Migration
             $table->string('budgetNumber');
             $table->string('poNumber');
             $table->string('assetPicture')->nullable();
-            $table->boolean('statusRequest');
-            $table->boolean('statusConfirmation');
-            $table->boolean('outstandingStatus');
-
         });
     }
 
@@ -56,6 +52,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cip');
+        Schema::dropIfExists('asset');
     }
 };
